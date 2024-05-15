c3c2428bf9f682f1653dac6a66275da6606bb4ac
Wille Marcel <wille.yyz@gmail.com>
2024-05-15 11:39:34 -0300

    Keep process running for a minimum time interval (#16)

--- update.js
+++ update.js
@@ -1,9 +1,10 @@
 'use strict';
+const moment = require('moment');
 const { getPlanetTimestamp } = require('./util/get-states');
 const { getLastProcessedState, setProcessedState, storePendingReplications, getReplicationToProcess } = require('./util/redis-client');
 const { range } = require('./util/range');
 const run = require('./index');
-const { NUM_WORKERS } = require('./lib/constants');
+const { NUM_WORKERS, RESTART_INTERVAL } = require('./lib/constants');
 
 // Check the queue and pick a file to process
 const processReplication = async () => {
@@ -21,7 +22,7 @@ const processReplication = async () => {
   }
 };
 
-const process = async () => {
+const queueAndProcess = async () => {
   // check if there are any files to process and queue them
   const planetState = await getPlanetTimestamp();
 
@@ -40,5 +41,14 @@ const process = async () => {
       async () => await processReplication()
   ));
 };
+
+const process = async () => {
+  const nextRestartTime = moment.utc().add(RESTART_INTERVAL, 'minute');
+  // Kubernetes workers need at least 5 minutes of interval between restarts
+  // So we've added the option to keep the process running for a time interval
+  while (moment.utc() < nextRestartTime) {
+    await queueAndProcess();
+  }
+}
 
 process();
