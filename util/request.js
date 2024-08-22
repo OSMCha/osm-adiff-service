527595b9a52d13e230c758bcfe8c6a69be639171
Jake Low <hello@jakelow.com>
2024-08-22 16:40:12 -0700

    Add User-Agent header to outgoing HTTP requests
    Excludes requests made by the AWS SDK to read/write data in S3, but
    includes all other HTTP requests made during operation, e.g. when
    getting changeset metadata from the OSM API, or when querying the
    Overpass server.

--- util/request.js
+++ util/request.js
@@ -1,49 +1,23 @@
-const https = require('https');
+const fetch = require('node-fetch');
+const { version } = require("../package.json");
 
-const request = async (url, method = 'GET', postData, token) => {
-  const [h, ...rest] = url.split('://')[1].split('/');
-  const path = `/${rest.join('/')}`;
-  const [host, port] = h.split(':');
+async function request(url, options = {}) {
+  if (!options.headers) {
+    options.headers = {}
+  }
 
-  const params = {
-    method,
-    host,
-    port: port || url.startsWith('https://') ? 443 : 80,
-    path: path || '/',
-  };
+  options.headers['User-Agent'] = `OSMCha osm-adiff-service ${version}`
 
-  if (token && postData) params.headers = {
-    Authorization: token,
-    'Content-Type': 'application/json',
-    'Content-Length' : Buffer.byteLength(postData, 'utf8')
-  };
+  let res = await fetch(url, options);
 
+  if (res.ok) {
+    return res;
+  } else {
+    let error = new Error(`HTTP ${res.status} ${res.statusText}`);
+    error.status = res.status;
+    error.statusText = res.statusText;
+    throw error;
+  }
+}
 
-  return new Promise((resolve, reject) => {
-    const req = https.request(params, res => {
-      if (res.statusCode < 200 || res.statusCode >= 303) {
-        return reject(new Error(`Status Code: ${res.statusCode}`));
-      }
-
-      const data = [];
-
-      res.on('data', chunk => {
-        data.push(chunk);
-      });
-
-      res.on('end', () => resolve(Buffer.concat(data).toString()));
-    });
-
-    req.on('error', reject);
-
-    if (postData) {
-      req.write(postData);
-    }
-
-    req.end();
-  });
-};
-
-module.exports =  {
-  request,
-};
+module.exports =  { request };
